71809b86e744ff2e94b9e1f74cd858904cf220a7
ste-scholes <68502614+ste-scholes@users.noreply.github.com>
2021-02-01 00:08:13 +0000

    update

---
+++ dom-code/dom.js
@@ -0,0 +1,10 @@
+// page1.html
+const img = document.getElementById('winter');
+
+console.dir(img)
+
+const elements = document.getElementsByTagName('input');
+
+console.dir(elements);
+
+console.log(elements[1])
