27fdc8413d4cc39957231c1cbf23cdd0e530cdb1
ste-scholes <68502614+ste-scholes@users.noreply.github.com>
2021-02-17 01:06:41 +0000

    1:31

--- dom-code/iterating.js
+++ dom-code/iterating.js
@@ -100,3 +100,69 @@ firstP.appendChild(newLink);
 newLink.classList.add('white')
 
 // 1:17
+// appendChild new LI
+
+
+const parentUL = document.querySelector('ul')
+console.log(parentUL)
+const newLI = document.createElement('li')
+console.log(newLI)
+
+newLI.innerText = 'Feed the Cat'
+parentUL.appendChild(newLI)
+
+// inserBefore
+
+const firstLI = document.querySelector('li.todo')
+
+parentUL.insertBefore(newLI, firstLI)
+// insert above last element
+const lastUL = document.querySelectorAll('li.todo')[2]
+parentUL.insertBefore(newLI, lastUL)
+
+// alternate way of doing the above
+
+const i = document.createElement('i')
+i.innerText = 'I am Italics '
+
+
+// first p tag on page
+// const firstP = document.querySelector('p')
+
+firstP.insertAdjacentElement('afterbegin', i)
+
+// 1:25
+
+// append
+
+firstP.append(i, newLI)
+firstP.prepend(i, newLI)
+
+const pNew = document.querySelector('p.new')
+
+const  text = document.createElement('b')
+text.innerText = 'Here is some text'
+
+pNew.append(i, text)
+
+// remove child (remove peas from list)
+
+// target parent - in a section and a ul => PARENT
+
+const ulPeas = document.querySelector('section ul')
+
+// target item you want removing - note ulPeas.querySelector => CHILD
+
+const removePeas = ulPeas.querySelector('.special')
+
+// remove element
+
+// pattern -> parent -> remove child => child
+
+const deleted = ulPeas.removeChild(removePeas)
+
+// the removed child is returnd in the variable
+
+console.log(deleted)
+
+// 1:31
